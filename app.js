ce4b54144d12f2f27ba385e495a82d15836ea46f
kurisuamadeus <28626750+ts-tatsuya@users.noreply.github.com>
2022-08-16 01:03:51 +0900

    Update 1
    - Added "Access-Control-Allow" header

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 3001;
 const jsonUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json';
 
-
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET")
+	next();
+  });
 /**JSON FORMAT
  * 
  * {
